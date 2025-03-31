cc5a1d1342e5e80c2f6c4eecc236e95e4d09dc1b
josephydu <josephyou@tencent.com>
2025-03-31 20:33:11 +0800

    fix nsa

--- tests/test_compression_attention.py
+++ tests/test_compression_attention.py
@@ -37,7 +37,7 @@ ref_o, ref_s = attention_ref(q, ck, cv, compress_block_stride, compress_block_si
 o, s = flash_attn_func(q, ck, cv, compress_block_stride, compress_block_size, True, None)
 
 print(ref_o.isnan().any())
-print(o.isnan().sum())
+print(o.isnan().any())
 # #import pdb; pdb.set_trace()
 
 torch.testing.assert_close(o, ref_o, rtol=1e-2, atol=1e-2)
