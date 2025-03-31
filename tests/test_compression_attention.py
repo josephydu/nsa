98ab0da379a085a9fa3954972936d8afe8bbf7d5
josephydu <josephyou@tencent.com>
2025-03-31 20:32:35 +0800

    fix nsa

--- tests/test_compression_attention.py
+++ tests/test_compression_attention.py
@@ -7,7 +7,7 @@ from nsa.compression_kv import KVCompressor
 bs, num_q_head, num_kv_head, head_dim = 1, 64, 4, 128
 compress_block_size, compress_block_stride = 64, 16
 selection_block, selected_block_count = 64, 32
-seq_len = 1024*16
+seq_len = 1024*32
 
 dtype = torch.bfloat16
 device = "cuda"
@@ -36,8 +36,8 @@ q = q.reshape(bs, seq_len, num_q_head, head_dim)
 ref_o, ref_s = attention_ref(q, ck, cv, compress_block_stride, compress_block_size, causal=True, scale=None)
 o, s = flash_attn_func(q, ck, cv, compress_block_stride, compress_block_size, True, None)
 
-# print(ref_o.isnan().any())
-# print(o.isnan().sum())
+print(ref_o.isnan().any())
+print(o.isnan().sum())
 # #import pdb; pdb.set_trace()
 
 torch.testing.assert_close(o, ref_o, rtol=1e-2, atol=1e-2)
