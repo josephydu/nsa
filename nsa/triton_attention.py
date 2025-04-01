0e8f3557c4127fd87cffe3febcee06a6eb9c5c58
josephydu <josephyou@tencent.com>
2025-04-01 15:54:46 +0800

    fix kv ptrs

--- nsa/triton_attention.py
+++ nsa/triton_attention.py
@@ -265,8 +265,14 @@ def _attn_bwd_dq(dq, q, K, V,  #
     curr_n = start_n
     step_n = BLOCK_N2
     for blk_idx in range(num_steps):
-        kT = tl.load(kT_ptrs, mask=(offs_n < KV_CTX)[None, :], other=0.0)
-        vT = tl.load(vT_ptrs, mask=(offs_n < KV_CTX)[None, :], other=0.0)
+        # NOTE: for q seq len is not the same as KV_CTX
+        _curr_n = (curr_n + blk_idx * BLOCK_N2) % KV_CTX
+        _offs_n = _curr_n + tl.arange(0, BLOCK_N2)
+        
+        kT = tl.load(K + _curr_n * k_stride_tok + offs_k[:, None] * k_stride_d, 
+                    mask=(_offs_n < KV_CTX)[None, :], other=0.0)
+        vT = tl.load(V + _curr_n * v_stride_tok + offs_k[:, None] * v_stride_d,
+                    mask=(_offs_n < KV_CTX)[None, :], other=0.0)
         qk = tl.dot(q, kT)
         p = tl.math.exp2(qk - m)
         # Autoregressive masking.
@@ -298,7 +304,7 @@ def _attn_bwd(Q, K, V, sm_scale,  #
                 stride_z, stride_h, stride_tok, stride_d,  #
                 k_stride_z, k_stride_h, k_stride_tok, k_stride_d,
                 v_stride_z, v_stride_h, v_stride_tok, v_stride_d,
-                H, N_CTX,  #
+                H, N_CTX, KV_CTX, #
                 BLOCK_M1: tl.constexpr,  #
                 BLOCK_N1: tl.constexpr,  #
                 BLOCK_M2: tl.constexpr,  #
@@ -312,16 +318,17 @@ def _attn_bwd(Q, K, V, sm_scale,  #
     bhid = tl.program_id(2)
     off_chz = (bhid * N_CTX).to(tl.int64)
     adj = (stride_h * (bhid % H) + stride_z * (bhid // H)).to(tl.int64)
+    adj_kv = (k_stride_h * (bhid % H) + k_stride_z * (bhid // H)).to(tl.int64)
     pid = tl.program_id(0)
 
     # offset pointers for batch/head
     Q += adj
-    K += adj
-    V += adj
+    K += adj_kv
+    V += adj_kv
     DO += adj
     DQ += adj
-    DK += adj
-    DV += adj
+    DK += adj_kv
+    DV += adj_kv
     M += off_chz
     D += off_chz
 
@@ -330,53 +337,6 @@ def _attn_bwd(Q, K, V, sm_scale,  #
 
     start_n = pid * BLOCK_N1
     start_m = start_n
-
-    MASK_BLOCK_M1: tl.constexpr = BLOCK_M1 // BLK_SLICE_FACTOR
-    offs_n = start_n + tl.arange(0, BLOCK_N1)
-
-    dv = tl.zeros([BLOCK_N1, HEAD_DIM], dtype=tl.float32)
-    dk = tl.zeros([BLOCK_N1, HEAD_DIM], dtype=tl.float32)
-
-    # load K and V: they stay in SRAM throughout the inner loop.
-    k = tl.load(K + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d)
-    v = tl.load(V + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d)
-
-    num_steps = BLOCK_N1 // MASK_BLOCK_M1
-
-    dk, dv = _attn_bwd_dkdv(dk, dv,  #
-                            Q, k, v, sm_scale,  #
-                            DO,  #
-                            M, D,  #
-                            stride_tok, stride_d,  #
-                            H, N_CTX,  #
-                            MASK_BLOCK_M1, BLOCK_N1, HEAD_DIM,  #
-                            start_n, start_m, num_steps,  #
-                            MASK=True  #
-                            )
-
-    start_m += num_steps * MASK_BLOCK_M1
-    num_steps = (N_CTX - start_m) // BLOCK_M1
-
-    # Compute dK and dV for non-masked blocks.
-    dk, dv = _attn_bwd_dkdv(  #
-        dk, dv,  #
-        Q, k, v, sm_scale,  #
-        DO,  #
-        M, D,  #
-        stride_tok, stride_d,  #
-        H, N_CTX,  #
-        BLOCK_M1, BLOCK_N1, HEAD_DIM,  #
-        start_n, start_m, num_steps,  #
-        MASK=False  #
-    )
-
-    dv_ptrs = DV + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d
-    tl.store(dv_ptrs, dv)
-
-    # Write back dK.
-    dk *= sm_scale
-    dk_ptrs = DK + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d
-    tl.store(dk_ptrs, dk)
 
     # THIS BLOCK DOES DQ:
     start_m = pid * BLOCK_M2
@@ -398,33 +358,33 @@ def _attn_bwd(Q, K, V, sm_scale,  #
     # not due to anything important.  I just wanted to reuse the loop
     # structure for dK & dV above as much as possible.
     num_steps = BLOCK_M2 // MASK_BLOCK_N2
-    # dq = _attn_bwd_dq(dq, q, K, V,  #
-    #                   do, m, D,  #
-    #                   stride_tok, stride_d,  #
-    #                   k_stride_tok, k_stride_d,
-    #                   v_stride_tok, v_stride_d,
-    #                   H, N_CTX,k.shape[1],  #
-    #                   BLOCK_M2, MASK_BLOCK_N2, HEAD_DIM,  #
-    #                   start_m, end_n - num_steps * MASK_BLOCK_N2, num_steps,  #
-    #                   block_stride=block_stride,
-    #                   block_size=block_size,
-    #                   MASK=True  #
-    #                   )
+    dq = _attn_bwd_dq(dq, q, K, V,  #
+                      do, m, D,  #
+                      stride_tok, stride_d,  #
+                      k_stride_tok, k_stride_d,
+                      v_stride_tok, v_stride_d,
+                      H, N_CTX,KV_CTX,  #
+                      BLOCK_M2, MASK_BLOCK_N2, HEAD_DIM,  #
+                      start_m, end_n - num_steps * MASK_BLOCK_N2, num_steps,  #
+                      block_stride=block_stride,
+                      block_size=block_size,
+                      MASK=True  #
+                      )
     end_n -= num_steps * MASK_BLOCK_N2
     # stage 2
     num_steps = end_n // BLOCK_N2
-    # dq = _attn_bwd_dq(dq, q, K, V,  #
-    #                   do, m, D,  #
-    #                   stride_tok, stride_d,  #
-    #                   k_stride_tok, k_stride_d,
-    #                   v_stride_tok, v_stride_d,
-    #                   H, N_CTX,k.shape[1],  #
-    #                   BLOCK_M2, BLOCK_N2, HEAD_DIM,  #
-    #                   start_m, end_n - num_steps * BLOCK_N2, num_steps,  #
-    #                   block_stride=block_stride,
-    #                   block_size=block_size,
-    #                   MASK=False  #
-    #                   )
+    dq = _attn_bwd_dq(dq, q, K, V,  #
+                      do, m, D,  #
+                      stride_tok, stride_d,  #
+                      k_stride_tok, k_stride_d,
+                      v_stride_tok, v_stride_d,
+                      H, N_CTX,KV_CTX,  #
+                      BLOCK_M2, BLOCK_N2, HEAD_DIM,  #
+                      start_m, end_n - num_steps * BLOCK_N2, num_steps,  #
+                      block_stride=block_stride,
+                      block_size=block_size,
+                      MASK=False  #
+                      )
     # Write back dQ.
     dq_ptrs = DQ + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d
     dq *= LN2
