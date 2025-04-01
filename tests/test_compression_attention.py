c8b297ccf1b736bfd743b075699dc312a56fe626
josephydu <josephyou@tencent.com>
2025-04-01 18:27:38 +0800

    fix kv ptrs

--- tests/test_compression_attention.py
+++ tests/test_compression_attention.py
@@ -83,8 +83,8 @@ loss = (o*o).sum()
 loss.backward()
 
 # print(q.grad.shape)
-print(q.grad[:1][:10])
-print(q_ref.grad[:1][:10])
+print(f'q.grad{q.grad[:1][:10]}')
+print(f'q_ref.grad{q_ref.grad[:1][:10]}')
 
 
 # torch.testing.assert_close(s, ref_s, rtol=1e-2, atol=1e-2)
