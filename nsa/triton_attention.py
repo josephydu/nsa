9a60056e360a9c2b692e1b0539455b6f35d2c55f
josephydu <josephyou@tencent.com>
2025-04-01 20:56:10 +0800

    fix kv ptrs

--- nsa/triton_attention.py
+++ nsa/triton_attention.py
@@ -260,7 +260,7 @@ def _attn_bwd_dq(dq, q, K, V,  #
     kT_ptrs = K + offs_n[None, :] * stride_tok + offs_k[:, None] * stride_d
     vT_ptrs = V + offs_n[None, :] * stride_tok + offs_k[:, None] * stride_d
     # D (= delta) is pre-divided by ds_scale.
-    Di = tl.load(D + offs_m*H)
+    Di = tl.load(D + offs_m)
     # BLOCK_M2 must be a multiple of BLOCK_N2, otherwise the code wouldn't work.
     tl.static_assert(BLOCK_M2 % BLOCK_N2 == 0)
     curr_n = start_n
@@ -491,7 +491,7 @@ class _attention(torch.autograd.Function):
             o, do,  #
             delta,  #
             BATCH, Q_HEAD, Q_CTX,  #
-            o.stride(0), o.stride(2), o.stride(1), o.stride(3),  # Add proper strides
+            o.stride(0), o.stride(2), o.stride(1), o.stride(3),  
             BLOCK_M=PRE_BLOCK, HEAD_DIM=ctx.HEAD_DIM  #
         )
         grid = (Q_CTX // BLOCK_N1, BATCH, Q_HEAD)
