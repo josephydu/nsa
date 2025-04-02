e8996c6a9ba5a9e71cbeccfec73803e5e2844e94
josephydu <josephyou@tencent.com>
2025-04-02 15:01:38 +0800

    init

--- nsa/nsa_triton.py
+++ nsa/nsa_triton.py
@@ -861,7 +861,7 @@ def parallel_nsa(q: torch.Tensor,
 
 if __name__ == "__main__":
     B, T, H, HQ, D, S, block_size, dtype = 2, 64, 1, 16, 32, 1, 32, torch.float16
-    # torch.random.manual_seed(42)
+    torch.random.manual_seed(42)
     q = torch.randn((B, T, HQ, D), dtype=dtype, device='cuda').requires_grad_(True)
     k = torch.randn((B, T, H, D), dtype=dtype, device='cuda').requires_grad_(True)
     v = torch.randn((B, T, H, D), dtype=dtype, device='cuda').requires_grad_(True)
@@ -878,6 +878,7 @@ if __name__ == "__main__":
     block_indices = block_indices.sort(-1)[0]
 
     block_counts = torch.randint(1, S + 1, (B, T, H), device='cuda')
+    print(block_counts)
 
     ref = naive_nsa(
         q=q,
