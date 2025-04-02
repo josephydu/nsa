e25518d3fadd5222bb061f9390b8c15231f4a8a8
josephydu <josephyou@tencent.com>
2025-04-02 18:35:56 +0800

    add test_parallel_nsa.py for tests nsa

--- tests/test_parallel_nsa.py
+++ tests/test_parallel_nsa.py
@@ -221,8 +221,7 @@ if __name__ == "__main__":
     )
     
 
-    # ref[0][0] = 0.0
-    # ref[1][63] = 0.0
+
 
     tri.backward(do)
     tri_dq, q.grad = q.grad.clone(), None
@@ -231,6 +230,15 @@ if __name__ == "__main__":
     tri_dg_slc, g_slc.grad = g_slc.grad.clone(), None
 
 
+
+    
+    # assert_close(" o", ref, tri, 0.004)
+    torch.testing.assert_close(ref, tri, atol=1e-2, rtol=1e-2)
+    torch.testing.assert_close(ref_dq, tri_dq, atol=1e-2, rtol=1e-2)
+    torch.testing.assert_close(ref_dk, tri_dk, atol=1e-2, rtol=1e-2)
+    torch.testing.assert_close(ref_dv, tri_dv, atol=1e-2, rtol=1e-2)
+    torch.testing.assert_close(ref_dg_slc, tri_dg_slc, atol=1e-2, rtol=1e-2)
+    
     assert not torch.isnan(ref).any()
     assert not torch.isnan(ref_dq).any()
     assert not torch.isnan(ref_dk).any()
@@ -242,10 +250,3 @@ if __name__ == "__main__":
     assert not torch.isnan(tri_dk).any()
     assert not torch.isnan(tri_dv).any()
     assert not torch.isnan(tri_dg_slc).any()
-    
-    # assert_close(" o", ref, tri, 0.004)
-    torch.testing.assert_close(ref, tri, atol=1e-2, rtol=1e-2)
-    torch.testing.assert_close(ref_dq, tri_dq, atol=1e-2, rtol=1e-2)
-    torch.testing.assert_close(ref_dk, tri_dk, atol=1e-2, rtol=1e-2)
-    torch.testing.assert_close(ref_dv, tri_dv, atol=1e-2, rtol=1e-2)
-    torch.testing.assert_close(ref_dg_slc, tri_dg_slc, atol=1e-2, rtol=1e-2)
