582518c727b923ed1e61d192b0ec6b9b87e3406d
josephydu <josephyou@tencent.com>
2025-04-02 15:18:15 +0800

    init

--- nsa/reference.py
+++ nsa/reference.py
@@ -136,7 +136,7 @@ def naive_nsa(q: torch.Tensor,
             attn_slc = torch.einsum('h d, n h d -> n h', q_i, k_i_slc).masked_fill(
                 torch.logical_or(i_i < 0, i_i > i_q) |
                 (c >= s_i if block_counts is not None else False), float('-inf'))
-            print(attn_slc)
+            print(attn_slc.shape)
             attn_slc = attn_slc.softmax(dim=0)
             if not varlen:
                 o_slc[i, i_q] = torch.einsum('n h, n h v -> h v', attn_slc,
