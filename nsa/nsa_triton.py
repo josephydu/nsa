dc9469379909db43c91f0936b1c6d96acc399acb
josephydu <josephyou@tencent.com>
2025-04-02 14:35:32 +0800

    init

--- nsa/nsa_triton.py
+++ nsa/nsa_triton.py
@@ -522,8 +522,9 @@ def parallel_nsa_fwd_kernel(q, k, v, o_slc, o_swa, lse_slc, lse_swa, scale, bloc
             b_o_slc = b_o_slc * b_r_slc[:, None] + tl.dot(b_p_slc.to(b_q.dtype), b_v_slc)
 
             b_mp_slc = b_m_slc
-    b_o_slc = b_o_slc / b_acc_slc[:, None]
-    b_m_slc += tl.log(b_acc_slc)
+    if flag:
+        b_o_slc = b_o_slc / b_acc_slc[:, None]
+        b_m_slc += tl.log(b_acc_slc)
     tl.store(p_o_slc, b_o_slc.to(p_o_slc.dtype.element_ty), boundary_check=(0, 1))
     tl.store(p_lse_slc, b_m_slc.to(p_lse_slc.dtype.element_ty))
 
