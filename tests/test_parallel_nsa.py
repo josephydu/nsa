6ab9f551bd22be4d0f0f8ebda13d286a7ac9a3fa
josephydu <josephyou@tencent.com>
2025-04-02 17:06:29 +0800

    add test_parallel_nsa.py for tests nsa

--- tests/test_parallel_nsa.py
+++ tests/test_parallel_nsa.py
@@ -266,10 +266,8 @@ if __name__ == "__main__":
     assert not torch.isnan(tri_dg_slc).any()
     
     # assert_close(" o", ref, tri, 0.004)
-    import pdb;
-    pdb.set_trace()
     torch.testing.assert_close(ref, tri, atol=1e-2, rtol=1e-2)
     torch.testing.assert_close(ref_dq, tri_dq, atol=1e-2, rtol=1e-2)
     torch.testing.assert_close(ref_dk, tri_dk, atol=1e-2, rtol=1e-2)
-    # torch.testing.assert_close(ref_dv, tri_dv, atol=1e-2, rtol=1e-2)
-    # torch.testing.assert_close(ref_dg_slc, tri_dg_slc, atol=1e-2, rtol=1e-2)
+    torch.testing.assert_close(ref_dv, tri_dv, atol=1e-2, rtol=1e-2)
+    torch.testing.assert_close(ref_dg_slc, tri_dg_slc, atol=1e-2, rtol=1e-2)
