3e5cef8a2e7d16ada77c4e14b52ef0c7208f76a4
josephydu <josephyou@tencent.com>
2025-04-02 17:07:29 +0800

    add test_parallel_nsa.py for tests nsa

--- tests/test_parallel_nsa.py
+++ tests/test_parallel_nsa.py
@@ -187,7 +187,7 @@ def naive_nsa(q: torch.Tensor,
 
 if __name__ == "__main__":
     B, T, H, HQ, D, S, block_size, dtype = 2, 64, 1, 16, 32, 2, 32, torch.float16
-    # torch.random.manual_seed(84831)
+    torch.random.manual_seed(0)
     q = torch.randn((B, T, HQ, D), dtype=dtype, device='cuda').requires_grad_(True)
     k = torch.randn((B, T, H, D), dtype=dtype, device='cuda').requires_grad_(True)
     v = torch.randn((B, T, H, D), dtype=dtype, device='cuda').requires_grad_(True)
