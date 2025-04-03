4f8d523a4b99432470f11b303bacd0a0080e0d3b
josephydu <josephyou@tencent.com>
2025-04-03 11:26:54 +0800

    test

--- tests/test_parallel_nsa.py
+++ tests/test_parallel_nsa.py
@@ -248,6 +248,7 @@ if __name__ == "__main__":
     tri_dv, v.grad = v.grad.clone(), None
     tri_dg_slc, g_slc.grad = g_slc.grad.clone(), None
     
+    import pdb; pdb.set_trace()
     # assert not torch.isnan(ref).any()
     # assert not torch.isnan(ref_dq).any()
     # assert not torch.isnan(ref_dk).any()
